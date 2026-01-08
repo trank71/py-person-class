a23b7857c9b79a4c5de68b70e709c6c4514b2c5a
Ambra <trank13f@gmail.com>
2026-01-08 18:39:53 +0100

    add  wife/husband atribut

--- app/main.py
+++ app/main.py
@@ -12,5 +12,12 @@ def create_person_list(people: list) -> list:
     for person in people:
         one_person = Person(person["name"], person["age"])
         person_list.append(one_person)
+    for family in people:
+        person = Person.people[family["name"]]
 
+        if "wife" in family and family["wife"] is not None:
+            person.wife = Person.people[family["wife"]]
+
+        if "husband" in family and family["husband"] is not None:
+            person.husband = Person.people[family["husband"]]
     return person_list
