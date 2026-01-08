e03a9688cd5bfaeddd61e3d4ef59fb46a2b312c7
Ambra <trank13f@gmail.com>
2026-01-08 18:20:13 +0100

    create class PERSON and class atribut people

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
     # write your code here
